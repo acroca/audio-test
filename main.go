840a3430d68346729a8a6482931e6aa340f5b3fd
Albert Callarisa <shark234@gmail.com>
2015-12-12 22:37:15 -0800

    moving to openal

--- main.go
+++ main.go
@@ -1,27 +1,60 @@
 package main
 
 import (
+	"math"
 	"time"
 
 	g "github.com/acroca/audio-test/generators"
 	"github.com/gordonklaus/portaudio"
+	"github.com/timshannon/go-openal/openal"
 )
 
-const sampleRate = 44100
+const (
+	sampleRate = 44100
+	// maxInt16   = int16((1 << 15) - 1)
+	maxInt16 = 32760
+)
 
 type audioGen interface {
 	ProcessAudio(out [][]float32)
 }
 
 func main() {
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-	s := newStereo()
-	defer s.Close()
-	chk(s.Start())
+	d := openal.OpenDevice(openal.GetString(openal.DefaultDeviceSpecifier))
+	ctx := d.CreateContext()
+	ctx.Activate()
+
+	s := openal.NewSource()
+	stereo := newStereo()
+	var arr = make([][]float32, 2)
+	arr[0] = make([]float32, sampleRate)
+	arr[1] = make([]float32, sampleRate)
+	var arrI = make([][2]int16, sampleRate)
+	for i := 0; i < 3; i++ {
+		b := openal.NewBuffer()
+		stereo.processAudio(arr)
+		for i := 0; i < sampleRate; i++ {
+			arrI[i][0] = int16(math.Floor(float64(arr[0][i] * float32(maxInt16))))
+			arrI[i][1] = int16(math.Floor(float64(arr[1][i] * float32(maxInt16))))
+		}
+		b.SetDataStereo16(arrI, sampleRate)
+		s.QueueBuffer(b)
+	}
+
+	s.Play()
 	time.Sleep(5 * time.Second)
-	chk(s.Stop())
 }
+
+// func main2() {
+// 	portaudio.Initialize()
+// 	defer portaudio.Terminate()
+// 	s := newStereo()
+// 	s.play()
+// 	defer s.Close()
+// 	chk(s.Start())
+// 	time.Sleep(5 * time.Second)
+// 	chk(s.Stop())
+// }
 
 type stereo struct {
 	*portaudio.Stream
@@ -72,12 +105,14 @@ func newStereo() *stereo {
 			),
 		),
 	)
-	var err error
-	s.Stream, err = portaudio.OpenDefaultStream(0, 2, sampleRate, 0, s.processAudio)
-	chk(err)
 	return s
 }
 
+func (s *stereo) play() {
+	var err error
+	s.Stream, err = portaudio.OpenDefaultStream(0, 2, sampleRate, 0, s.processAudio)
+	chk(err)
+}
 func (g *stereo) processAudio(out [][]float32) {
 	g.channel.ProcessAudio(out)
 }
